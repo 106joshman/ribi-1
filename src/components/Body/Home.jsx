f3b7d2f32c6762104ab6d2abf948b5f30ee58746
Joshman <ejembijoshman@gmail.com>
2021-12-09 15:30:08 +0100

    home page updates

--- src/components/Body/Home.jsx
+++ src/components/Body/Home.jsx
@@ -1,20 +1,45 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
 import "./Home.css";
+import image from '../images/Group.png';
+import images from '../images/Vector.png';
 
 const Home = () => {
     return (
-        <div className="hero-body">
-            <div className="hero-left">
-                <h1>Learn To Give Blood</h1>
-                <p className="text">
-                    Register today and help save a live
-                </p>
-                <button>
-                    Learn More <AiOutlineArrowRight />
-                </button>
+        <div className="bodi">
+            <div className="hero-body">
+                <div className="hero-left">
+                    <h1>Learn To Give Blood</h1>
+                    <p className="text">
+                        Register today and help save a live
+                    </p>
+                    <button>
+                        Learn More <AiOutlineArrowRight />
+                    </button>
+                </div>
+                <div className="hero-right">
+                    <div className="back">
+                        <img src={images} alt="Ribi homapage display" srcset=""/>
+                            <div className="front">
+                                <img src={image} alt="Ribi homapage display" srcset=""/>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="hero-right">
-                <img src="/src/images/Group.png" alt="" srcset=""/>
+            <div className="types">
+                <h4>Blood Types</h4>
+            </div>
+            <div className="mission">
+                <p>Your donation will save lives. It’s not just an appointment, it’s a commitment to save lives!</p>
+                <div className="card one">
+                    <h6>HOW TO DONATE</h6>
+                    <p>Donating blood is a safe, simple and rewarding experience that usually takes less than an hour! Donating is easy, and it makes a huge difference in the lives of others.</p>
+                    <button>Learn More <AiOutlineArrowRight /></button>
+                </div>
+                <div className="card two">
+                    <h6>WHERE TO DONATE</h6>
+                    <p>We offer two types of locations for donations: donor centers and blood drives. We serve local hospitals in within Nigeria. </p>
+                    <button>Learn More <AiOutlineArrowRight /></button>
+                </div>
             </div>
         </div>
     );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,27 @@
 import './Footer.css';
+import logo from '../images/logo.svg';
+import { GrFacebookOption, GrLinkedinOption, GrTwitter } from "react-icons/gr";
 
 const Footer = () => {
     return (
         <footer>
-            the footer
+            <h5>DONATE BLOOD WHEN NEEDED</h5>
+            <p>Please book an appointment to donate. Appointments are available at some of our centres.</p>
+            <div className="bttn">
+                <button>👀 Insights</button>
+                <button>👋 Contact</button>
+            </div>
+            <img 
+                src={logo} 
+                alt="RIBI logo" 
+                srcset=""
+            />
+            <p>© 2021 RIBI. All Rights Reserved. </p>
+            <div className="socials">
+                <GrFacebookOption />
+                <GrLinkedinOption />
+                <GrTwitter />
+            </div>
         </footer>
     );
 }
