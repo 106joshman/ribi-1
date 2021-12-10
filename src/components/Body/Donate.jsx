8d49beb6bf529f84ae58d252deb93c000d330d1c
Joshman <ejembijoshman@gmail.com>
2021-12-10 14:52:23 +0100

    donate page completed

--- src/components/Body/Donate.jsx
+++ src/components/Body/Donate.jsx
@@ -2,20 +2,47 @@ import React from 'react';
 import styles from "./donate.module.css";
 import hero from "../../assets/hero.png";
 import profile1 from "../images/Ellipse.png";
+// import image from '../images/Blood.svg';
 
 const Donate = () => {
     return (
         <div className={styles.needBloodSection}>
-            <section className={styles.newBloodSection}>
-                <div className={styles.imageContainer}>
-                    <img className={styles.heroImg} src={hero} alt="hero" />
-                    <p className={styles.heroText}>DONOR INFO</p>
-                </div>
-            </section>
+            <div className={styles.imageContainer}>
+                <img className={styles.heroImg} src={hero} alt="hero" />
+                <p className={styles.heroText}>DONOR INFO</p>
+            </div>
             <div className={styles.findDonor}>
                 <div className={styles.donateBlood}>
                     <img className={styles.donorImage} src={profile1} alt="donor holder" />
-                    <h5 className={styles.profileName}>{}</h5>
+                    <h5 className={styles.profileName}>Dare Fatosho</h5>
+                    <p className={styles.profileNumber}>#1229</p>
+                </div>
+                <div className={styles.donorDesc}>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique diam aliquetmassa non quam augue
+                    </p>
+                </div>
+                <div className={styles.donorCard}>
+                    <div className={styles.donorLoc}>
+                        <p className={styles.loc}>Location</p>
+                        <h5 className={styles.donorLo}>Lagos State, Nigeria</h5>
+                        <h5 className={styles.donorLo}>Mushin</h5>
+                    </div>
+                    <div className={styles.donorDetail}>
+                        <div className={styles.donorStatus}>
+                            <p>Available</p>
+                            <h5>Available</h5>
+                        </div>
+                        <div className={styles.donorStatus}>
+                            <p>Phone</p>
+                            <h5>08023433443</h5>
+                        </div>
+                    </div>
+                    <div className={styles.bloodSample}>
+                        <p className={styles.bloodText}>A+</p>
+                    </div>
+                </div>
+                <div className={styles.bttn}>
                     <button className={styles.searchButton}>Request</button>
                 </div>
             </div>
