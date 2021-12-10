c99839cbd8b7400944abc0bc6a416bc09a2171a8
Oge Obubu <ogeobubu@gmail.com>
2021-12-10 10:14:11 +0100

    added bio-page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/Body/Home";
 import Need from "./components/Body/Need";
 import Donate from "./components/Body/Donate";
+import Bio from "./components/Body/Bio";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/need-blood" element={<Need />} />
           <Route path="/donate-blood" element={<Donate />} />
+          <Route path="/bio" element={<Bio />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
         <Footer />
