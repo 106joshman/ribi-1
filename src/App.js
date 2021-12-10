b813adc66232d13cbddbeb96fa3f2c6b344466bc
Oge Obubu <ogeobubu@gmail.com>
2021-12-10 12:28:02 +0100

    added signin and signup pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./components/Body/Home";
 import Need from "./components/Body/Need";
 import Donate from "./components/Body/Donate";
 import Bio from "./components/Body/Bio";
+import Signin from "./components/Body/Modal/Signin";
+import Signup from "./components/Body/Modal/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
           <Route path="/need-blood" element={<Need />} />
           <Route path="/donate-blood" element={<Donate />} />
           <Route path="/bio" element={<Bio />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
         <Footer />
