2bbbba502833ffa833f07c9fbf6a4ef9ae37eb9c
Oge Obubu <ogeobubu@gmail.com>
2022-01-20 19:58:14 +0100

    added redux functionality

---
+++ src/redux/userSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  // User Data State.
+  initialState: {
+    isLogged: false,
+    token: "",
+    user: {},
+  },
+  reducers: {
+    dispatchIsLogged: (state) => {
+      state.isLogged = true;
+    },
+    dispatchUserToken: (state, action) => {
+      state.token = action.payload;
+    },
+    dispatchUser: (state, action) => {
+      state.user = action.payload;
+    },
+    dispatchLogout: (state) => {
+      state.isLogged = false;
+    },
+  },
+});
+
+export const {
+  dispatchIsLogged,
+  dispatchUserToken,
+  dispatchUser,
+  dispatchLogout,
+} = userSlice.actions;
+export default userSlice.reducer;
