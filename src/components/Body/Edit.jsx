1235524ca67a4709f3694ca679dcddd5972d53d5
Oge Obubu <ogeobubu@gmail.com>
2022-01-21 06:22:22 +0100

    check to enable user edit profile

--- src/components/Body/Edit.jsx
+++ src/components/Body/Edit.jsx
@@ -6,90 +6,102 @@ import hero from "../../assets/hero.png";
 import donorPic from "../../assets/donorPic.png";
 import camera from "../../assets/camera.png";
 import editPen from "../../assets/editPen.png";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Edit = () => {
-  return (
-    <>
-      <section className={styles.bioSection}>
-        <div className={styles.imageContainer}>
-          <img className={styles.heroImg} src={hero} alt="hero" />
-          <p className={styles.heroText}>DONOR EDIT/UPDATE</p>
-        </div>
-      </section>
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user.user);
+  const token = useSelector((state) => state.user.token);
 
-      <section className={styles.bioContainer}>
-        <div className={styles.profilePicture}>
-          <div className={styles.profilePictureCon}>
-            <img
-              className={styles.profilePic}
-              src={donorPic}
-              alt="profilePicture"
-            />
+  if (!token) {
+    return navigate("/home");
+  } else {
+    return (
+      <>
+        <section className={styles.bioSection}>
+          <div className={styles.imageContainer}>
+            <img className={styles.heroImg} src={hero} alt="hero" />
+            <p className={styles.heroText}>DONOR EDIT/UPDATE</p>
+          </div>
+        </section>
+
+        <section className={styles.bioContainer}>
+          <div className={styles.profilePicture}>
+            <div className={styles.profilePictureCon}>
+              <img
+                className={styles.profilePic}
+                src={donorPic}
+                alt="profilePicture"
+              />
+            </div>
+
+            <label for="fusk" className={styles.editPhoto}>
+              <img className={styles.cameraIcon} src={camera} alt="camera" />
+              <span className={styles.editCameraText}>
+                Change Profile Photo
+              </span>
+            </label>
+            <input style={{ display: "none" }} type="file" id="fusk" />
           </div>
 
-          <label for="fusk" className={styles.editPhoto}>
-            <img className={styles.cameraIcon} src={camera} alt="camera" />
-            <span className={styles.editCameraText}>Change Profile Photo</span>
-          </label>
-          <input style={{ display: "none" }} type="file" id="fusk" />
-        </div>
-
-        <div className={styles.detailsContainer}>
-          <div className={styles.detailsHead}>
-            <div className={styles.grid}>
-              <div className={styles.details}>
-                <p className={styles.detailText}>First name:</p>
-                <p className={`${styles.flex} ${styles.detailText}`}>
-                  Dare
-                  <img src={editPen} alt="edit" />
-                </p>
-                <p className={styles.detailText}>Last name:</p>
-                <p className={`${styles.flex} ${styles.detailText}`}>
-                  Fatosho
-                  <img src={editPen} alt="edit" />
-                </p>
-                <p className={styles.detailText}>Phone number:</p>
-                <p className={`${styles.flex} ${styles.detailText}`}>
-                  0901234567789
-                  <img src={editPen} alt="edit" />
-                </p>
-                <p className={styles.detailText}>Blood Type:</p>
-                <p className={`${styles.flex} ${styles.detailText}`}>
-                  A+
-                  <img src={editPen} alt="edit" />
-                </p>
-                <p className={styles.detailText}>Country:</p>
-                <p className={`${styles.flex} ${styles.detailText}`}>
-                  Nigeria
-                  <img src={editPen} alt="edit" />
-                </p>
-                <p className={styles.detailText}>Home Address:</p>
-                <p className={`${styles.flex} ${styles.detailText}`}>
-                  3, Jolad Road, Ikeja
-                  <img src={editPen} alt="edit" />
-                </p>
-                <p className={styles.detailText}>State:</p>
-                <p className={`${styles.flex} ${styles.detailText}`}>
-                  Lagos
-                  <img src={editPen} alt="edit" />
-                </p>
-                <p className={styles.detailText}>City/Town:</p>
-                <p className={`${styles.flex} ${styles.detailText}`}>
-                  Ikeja
-                  <img src={editPen} alt="edit" />
-                </p>
-                <p className={styles.detailText}>Email:</p>
-                <p className={`${styles.flex} ${styles.detailText}`}>
-                  Darefatosho@gmail.com
-                  <img src={editPen} alt="edit" />
-                </p>
+          <div className={styles.detailsContainer}>
+            <div className={styles.detailsHead}>
+              <div className={styles.grid}>
+                <div className={styles.details}>
+                  <p className={styles.detailText}>First name:</p>
+                  <p className={`${styles.flex} ${styles.detailText}`}>
+                    {user?.firstname}
+                    <img src={editPen} alt="edit" />
+                  </p>
+                  <p className={styles.detailText}>Last name:</p>
+                  <p className={`${styles.flex} ${styles.detailText}`}>
+                    {user?.lastname}
+                    <img src={editPen} alt="edit" />
+                  </p>
+                  <p className={styles.detailText}>Phone number:</p>
+                  <p className={`${styles.flex} ${styles.detailText}`}>
+                    {user?.phone}
+                    <img src={editPen} alt="edit" />
+                  </p>
+                  <p className={styles.detailText}>Blood Type:</p>
+                  <p className={`${styles.flex} ${styles.detailText}`}>
+                    {user?.bloodType}
+                    <img src={editPen} alt="edit" />
+                  </p>
+                  <p className={styles.detailText}>Country:</p>
+                  <p className={`${styles.flex} ${styles.detailText}`}>
+                    {user?.country}
+                    <img src={editPen} alt="edit" />
+                  </p>
+                  <p className={styles.detailText}>Home Address:</p>
+                  <p className={`${styles.flex} ${styles.detailText}`}>
+                    {user?.homeAddress}
+                    <img src={editPen} alt="edit" />
+                  </p>
+                  <p className={styles.detailText}>State:</p>
+                  <p className={`${styles.flex} ${styles.detailText}`}>
+                    {user?.state}
+                    <img src={editPen} alt="edit" />
+                  </p>
+                  <p className={styles.detailText}>City/Town:</p>
+                  <p className={`${styles.flex} ${styles.detailText}`}>
+                    {user?.city}
+                    <img src={editPen} alt="edit" />
+                  </p>
+                  <p className={styles.detailText}>Email:</p>
+                  <p className={`${styles.flex} ${styles.detailText}`}>
+                    {user?.email}
+                    <img src={editPen} alt="edit" />
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </>
-  );
+        </section>
+      </>
+    );
+  }
 };
 
 export default Edit;
