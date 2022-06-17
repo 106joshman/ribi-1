d67c25ffd85381c71be039c7c3ce11b6a4431854
ejembi joshua <ejembijoshman@gmail.com>
2022-06-17 22:12:44 +0100

    available donor view page change data and design

--- src/components/Body/Donate.jsx
+++ src/components/Body/Donate.jsx
@@ -21,7 +21,7 @@ const Donate = () => {
       </section>
       <div className="findDonor mt-10 mb-20 text-white font-poppins w-4/5 mx-auto bg-thickred px-7 md:px-24 py-10 h-max">
         <div className="align-center text-center">
-          <img className="mx-auto" src={user.avater} alt={user.firstname} />
+          <img className="mx-auto rounded" src={user.avater} alt={user.firstname} />
           <h5 className="font-bold my-1">
             {user.firstname} {user.lastname}
           </h5>
@@ -30,7 +30,7 @@ const Donate = () => {
         <div className="donorCard rounded-3xl p-3.5 md:p-6 text-center bg-donate md:w-3/4 mt-8 mx-auto h-max">
           <div className="donorLoc mb-5">
             <p className="font-normal opacity-50">Location</p>
-            <h5 className="mt-2 font-bold">{user.homeAddress}</h5>
+            <h5 className="mt-2 font-bold">{user.donorLocation}</h5>
             <h5 className="font-bold">{user.city}</h5>
           </div>
           <div className="donorStatus border-t border-b border-opacity-50 p-3 mb-5">
@@ -38,7 +38,7 @@ const Donate = () => {
             <h5 className="">Available</h5>
           </div>
           <div className="drop mb-5 mx-auto">
-            <p className="text-pryclr text-underline text-center font-bold">
+            <p className="text-pryclr text-underline text-2xl text-center font-bold">
               {user.bloodType}
             </p>
           </div>
